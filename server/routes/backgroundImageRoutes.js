f96158d8d34838124ab6138defb639401d739ab2
Davie <daviehalogen@gmail.com>
2024-12-12 21:15:03 +0300

    Added client code to main branch

--- server/routes/backgroundImageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { backgroundImages } = require('../controllers/backgroundImageController');
-
-const router = express.Router();
-
-// Endpoint to get all background image URLs
-router.get('/', backgroundImages)
-
-module.exports = router;
